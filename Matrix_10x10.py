8757b150f40df27c2bafa83988d282c88298222d
Ashish Balpande <122813507+Ashish-Balpande@users.noreply.github.com>
2024-08-08 19:44:23 +0530

    Add files via upload

--- Matrix_10x10.py
+++ Matrix_10x10.py
@@ -1,18 +1,26 @@
 
 class matrix:
+    matrixflag = 3
     def __init__(self):
         self.row = None
         self.coloumn = None
         self.mat = []
         
-    def input(self, row, coloumn):
-        if (row > 10 or coloumn > 10) and (row < 1 or coloumn < 1):
-            print("Exceeded limits of 10")
-            #raise OverflowError 
+    def input(self):
+        self.matrixflag -= 1
+        self.row = int(input("Enter number of row: "))
+        self.coloumn = int(input("Enter number of coloumn: "))
+        
+        if (self.row <= 10 and self.coloumn <= 10) and (self.row >= 1 and self.coloumn >= 1):
+            self.createMatrix()        
         else:
-            self.row = row
-            self.coloumn = coloumn
-            self.createMatrix()
+            if self.matrixflag > 0:
+                print(f"\nExceeded limits of row and coloumn \nTry again... you have last {self.matrixflag} chance\n")
+                self.input()
+            else:
+                print("\nYou Exceeded the all 3 chances...\n", "_"*100)
+            #raise OverflowError 
+            
 
     def createMatrix(self):
         for r in range(self.row):
@@ -39,17 +47,21 @@ class matrix:
         self.printMatrix()
         
     def updateElement(self):
-        r1 = int(input("Enter the row number: "))
-        c1 = int(input("Enter the coloumn number: "))
+        print("Update the Matrix element")
+        r1 = int(input(f"\tEnter the row number(0-{self.row-1}): "))
+        c1 = int(input(f"\tEnter the coloumn number(0-{self.coloumn-1}): "))
         self.addElement(r1, c1)
     
 
 matObj = matrix()
 
-matObj.input(7, 9)
+matObj.input()
 matObj.printMatrix()
 
-matObj.updateElement()
+num=1
+while(num == 1):
+    matObj.updateElement()
+    num = input("Are you want to update again! \n\tPress 1: for update\n\tPress 0: for not update\n")
 
 
         
